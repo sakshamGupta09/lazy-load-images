584b7b69a3d495256d7de4e241762a72dc3167da
Saksham Gupta <saksham.09.gupta@gmail.com>
2022-12-20 09:58:42 +0530

    Initial template setup

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# laz-load-images
+# Infinite scrolling using Intersection Observer API
+
+[Demo](https://sakshamgupta-infinite-scrolling.vercel.app/)
+
+# Project description
+
+This is a vanilla JS project which uses [Unsplash API](https://unsplash.com/documentation) to fetch the most popular images and render the results onto the UI. It supports infinite scrolling using Intersection Observer API. The layout is completely responsive and supports dark theme as well.
+
+# Key Features
+
+- Infinite scrolling using Intersection Observer API.
+- Supports dark and light theme.
+- Automatically displays the page in user's preferred colour scheme (The OS settings).
+- Unsplash API is used for fetching records which supports pagination.
+- Completely responsive and elegant UI.
+- Uses fetch API for network calls.
+
+# Screenshots
+
+1). Dark Mode
+
+<img width="1728" alt="Screenshot 2022-12-20 at 7 35 05 AM" src="https://user-images.githubusercontent.com/49288222/208564061-757512e2-eef4-4b78-9b63-68b47a8558cb.png">
+
+2). Light Mode
+
+<img width="1728" alt="Screenshot 2022-12-20 at 7 35 11 AM" src="https://user-images.githubusercontent.com/49288222/208564161-d01e1322-b2d2-4f38-b1e1-54006ec6d01c.png">
+
+3). Mobile screen
+
+<img width="498" alt="Screenshot 2022-12-20 at 9 18 38 AM" src="https://user-images.githubusercontent.com/49288222/208579455-c0097760-9f96-4517-b7c9-beacd79cfe5e.png">
+
---
+++ scripts/index.js
@@ -0,0 +1,146 @@
+import debounceTime from "../utils/debounce.js";
+
+// State
+
+let photos = [];
+
+// Constants
+
+const CONSTANTS = {
+  API_URL: "https://api.unsplash.com",
+  API_KEY: "N02X3WfRqFebGeSgKmrJasrqXDXmeSal72qaFl4V8tI",
+  debounceDelay: 900,
+  themeLabels: {
+    dark: "Dark theme",
+    light: "Light theme",
+  },
+  paginationParams: {
+    per_page: 30,
+    page: 1,
+  },
+};
+
+// Dom Elements
+const cardsSectionElement = document.getElementById("images__container");
+
+const themeLabelElement = document.getElementById("theme-label");
+
+const themeIconElement = document.getElementById("theme-icon");
+
+const themeCheckboxElement = document.getElementById("theme-switch");
+
+// Event Listeners
+
+themeCheckboxElement.addEventListener("change", onThemeChange);
+
+// Functions
+
+async function fetchImages() {
+  const queryParams = getPayload();
+  try {
+    const apiURL = `${CONSTANTS.API_URL}/photos/?` + queryParams;
+    const response = await fetch(apiURL);
+    const data = await response.json();
+    data && setImages(data);
+  } catch (error) {
+    console.error(error);
+  } finally {
+  }
+}
+
+function getPayload() {
+  const queryParams = new URLSearchParams({
+    client_id: CONSTANTS.API_KEY,
+    per_page: CONSTANTS.paginationParams.per_page,
+    page: CONSTANTS.paginationParams.page,
+    order_by: "popular",
+  });
+
+  return queryParams.toString();
+}
+
+function setImages(response) {
+  const imagesWithDescription = response.filter(
+    (image) => image.description || image.alt_description
+  );
+  photos = imagesWithDescription;
+  renderUI();
+}
+
+function renderUI() {
+  photos.forEach((photo) => {
+    const cardNode = getCardNodeHTML(photo);
+    cardsSectionElement.appendChild(cardNode);
+  });
+}
+
+function getCardNodeHTML(photo) {
+  // Card container
+  const cardContainerElement = document.createElement("div");
+  setAttributesOfDomNode(cardContainerElement, { class: "card z-1" });
+
+  // Image
+  const imageElement = document.createElement("img");
+  setAttributesOfDomNode(imageElement, {
+    width: "100%",
+    class: "card__img",
+    src: photo.urls?.regular,
+    alt: photo.description || photo.alt_description,
+  });
+
+  // Card body
+  const cardBodyElement = document.createElement("div");
+  setAttributesOfDomNode(cardBodyElement, { class: "card__body font-bold" });
+
+  // Card title
+  const cardTitleElement = document.createElement("h4");
+  setAttributesOfDomNode(cardTitleElement, {
+    class: "text-2xl card__title break-word capitalize line-clamp-2",
+  });
+  cardTitleElement.textContent = photo.description || photo.alt_description;
+
+  // Card description
+  const cardDescriptionElement = document.createElement("p");
+  setAttributesOfDomNode(cardDescriptionElement, {
+    class: "card__description mt-4",
+  });
+  cardDescriptionElement.textContent = photo.user?.name;
+
+  cardBodyElement.append(cardTitleElement, cardDescriptionElement);
+  cardContainerElement.append(imageElement, cardBodyElement);
+
+  return cardContainerElement;
+}
+
+function setAttributesOfDomNode(domNode, attributes) {
+  for (let attributeName in attributes) {
+    domNode.setAttribute(attributeName, attributes[attributeName]);
+  }
+}
+
+function getSystemTheme() {
+  let isDarkMode = false;
+  if (window?.matchMedia("(prefers-color-scheme: dark)")?.matches) {
+    isDarkMode = true;
+  }
+  setTheme(isDarkMode);
+}
+
+function setTheme(isDarkMode) {
+  const themeLabel = isDarkMode ? "dark" : "light";
+  document.documentElement.setAttribute("data-theme", themeLabel);
+  themeLabelElement.textContent = CONSTANTS.themeLabels[themeLabel];
+  const iconClass = isDarkMode ? "far fa-moon" : "far fa-sun";
+  themeIconElement.setAttribute("class", iconClass);
+  themeCheckboxElement.checked = isDarkMode;
+}
+
+function onThemeChange(e) {
+  setTheme(e.target.checked);
+}
+
+// On load
+
+fetchImages();
+
+getSystemTheme();
